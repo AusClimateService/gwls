959bb50025d7c388580c219fa44c89d3ea357f05
Mitchell Black <mitchell.black@bom.gov.au>
2024-11-22 14:28:01 +1100

    correct call to read_GWL_yaml_file

--- gwl.py
+++ gwl.py
@@ -104,7 +104,7 @@ def get_GWL_lookup_table(CMIP):
         yaml file opened in safe mode (effectively a dictionary of dictinaries)
     """
 
-    yml = get_GWL_lookup_table('CMIP6')
+    yml = read_GWL_yaml_file('CMIP6')
     appended_df = []
     for gwl in yml.keys():
         df = pd.DataFrame(yml[gwl])
